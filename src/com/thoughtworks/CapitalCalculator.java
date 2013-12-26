10bad3d50330c5eb5f8a7192773ffbc77f5a6c11
cc <12apple@163.com>
2013-12-26 23:54:34 +0800

    add first test

--- src/com/thoughtworks/CapitalCalculator.java
+++ src/com/thoughtworks/CapitalCalculator.java
@@ -1,11 +1,18 @@
 package com.thoughtworks;
 
-public class CapitalComputer {
+public class CapitalCalculator {
     private static final double ADJ_FACTOR = 0.1;
     private double capital;
+    private double income;
     private double intRate;
     private double duration;
-    private double income;
+
+    public CapitalCalculator(double capital, double income, double intRate, double duration) {
+        this.capital = capital;
+        this.income = income;
+        this.intRate = intRate;
+        this.duration = duration;
+    }
 
     public double getAdjustedCapital(){
         double result = 0.0;
---
+++ test/com/thoughtworks/CapitalCalculatorTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CapitalCalculatorTest {
+    @Test
+    public void should_return_0_when_have_no_capital(){
+        CapitalCalculator calculator = new CapitalCalculator(0.0, 200, 0.1, 10);
+        assertThat(calculator.getAdjustedCapital(), is(0.0));
+    }
+    @Test
+    public void should_return_0_when_have_no_intRate(){
+        CapitalCalculator calculator = new CapitalCalculator(100, 200, 0.0, 10);
+        assertThat(calculator.getAdjustedCapital(), is(0.0));
+    }
+    @Test
+    public void should_return_0_when_have_no_duration(){
+        CapitalCalculator calculator = new CapitalCalculator(100, 200, 0.1, 0.0);
+        assertThat(calculator.getAdjustedCapital(), is(0.0));
+    }
+
+    @Test
+    public void should_calculate_result(){
+        CapitalCalculator calculator = new CapitalCalculator(100, 200, 0.1, 10);
+        assertThat(calculator.getAdjustedCapital(), is(2.0));
+    }
+}
