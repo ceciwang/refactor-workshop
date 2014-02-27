061f91fafdb87fb1ddd6020fc3955fe8447ac96c
wang chao <cwang@thoughtworks.com>
2014-02-27 16:49:04 +0800

    change example

--- src/com/thoughtworks/typecode/Employee2.java
+++ src/com/thoughtworks/typecode/Employee2.java
@@ -3,9 +3,14 @@ package com.thoughtworks.typecode;
 public class Employee2 {
 
     private int type;
+
     static final int ENGINEER = 0;
     static final int SALESMAN = 1;
     static final int MANAGER = 2;
+
+    private int monthlySalary;
+    private int commission;
+    private int bonus;
 
     public Employee2(int type) {
         this.type = type;
@@ -14,13 +19,27 @@ public class Employee2 {
     public int salary(){
         switch (type){
             case ENGINEER:
-                return 100;
+                return monthlySalary;
             case SALESMAN:
-                return 1000;
+                return monthlySalary + commission;
             case MANAGER:
-                return 10000;
+                return monthlySalary + bonus;
             default:
                 throw new RuntimeException("incorrect employee");
         }
     }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setCommission(int commission) {
+        this.commission = commission;
+    }
+
+    public void setMonthlySalary(int monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
 }
+
--- test/com/thoughtworks/typecode/Employee2Test.java
+++ test/com/thoughtworks/typecode/Employee2Test.java
@@ -9,19 +9,28 @@ public class Employee2Test {
     @Test
     public void should_return_salary_for_engineer(){
         Employee2 employee = new Employee2(Employee.ENGINEER);
-        assertThat(employee.salary(), is(100));
+        employee.setCommission(100);
+        employee.setMonthlySalary(200);
+        employee.setBonus(300);
+        assertThat(employee.salary(), is(200));
     }
 
     @Test
     public void should_return_salary_for_salesman(){
         Employee2 employee = new Employee2(Employee.SALESMAN);
-        assertThat(employee.salary(), is(1000));
+        employee.setCommission(100);
+        employee.setMonthlySalary(200);
+        employee.setBonus(300);
+        assertThat(employee.salary(), is(300));
     }
 
     @Test
     public void should_return_salary_for_manager(){
         Employee2 employee = new Employee2(Employee.MANAGER);
-        assertThat(employee.salary(), is(10000));
+        employee.setCommission(100);
+        employee.setMonthlySalary(200);
+        employee.setBonus(300);
+        assertThat(employee.salary(), is(500));
     }
 
 }
